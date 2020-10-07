90611b907d29660b6f5bb9216247eab0c02890dd
Ian Hundere <ianfundere@gmail.com>
2020-10-06 23:00:32 -0400

    Unleash to html beast.

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,49 +4,58 @@ import PropTypes from 'prop-types';
 import resume from '../images/IanHundereResume.pdf';
 
 const Header = (props) => (
-	<header id="header" style={props.timeout ? { display: 'none' } : {}}>
-		<div className="content">
-			<div className="inner">
-				<h1>Ian Hundere</h1>
-				<p>DevOps / SRE / Cloud</p>
-				<p>Engineer</p>
-			</div>
-		</div>
-		<nav>
-			<ul>
-				<li>
-					<a
-						href="javascript:;"
-						onClick={() => {
-							props.onOpenArticle('intro');
-						}}
-					>
-						Projects
-					</a>
-				</li>
-				<li>
-					<a
-						href="javascript:;"
-						onClick={() => {
-							props.onOpenArticle('work');
-						}}
-					>
-						About
-					</a>
-				</li>
-				<li>
-					<a href={resume} target="_blank" rel="noopener noreferrer">
-						Resume
-					</a>
-				</li>
-			</ul>
-		</nav>
-	</header>
+    <header id="header" style={props.timeout ? { display: 'none' } : {}}>
+        <div className="content">
+            <div className="inner">
+                <h1>Ian Hundere</h1>
+                <section align="left">
+                    <sub>
+                        DevOps{' '}
+                        <sub>
+                            SRE{' '}
+                            <sub>
+                                Cloud <sub>Engineer </sub>
+                            </sub>
+                        </sub>
+                    </sub>
+                </section>
+            </div>
+        </div>
+        <nav>
+            <ul>
+                <li>
+                    <a
+                        href="javascript:;"
+                        onClick={() => {
+                            props.onOpenArticle('intro');
+                        }}
+                    >
+                        Projects
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href="javascript:;"
+                        onClick={() => {
+                            props.onOpenArticle('work');
+                        }}
+                    >
+                        About
+                    </a>
+                </li>
+                <li>
+                    <a href={resume} target="_blank" rel="noopener noreferrer">
+                        Resume
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </header>
 );
 
 Header.propTypes = {
-	onOpenArticle: PropTypes.func,
-	timeout: PropTypes.bool
+    onOpenArticle: PropTypes.func,
+    timeout: PropTypes.bool,
 };
 
 export default Header;
