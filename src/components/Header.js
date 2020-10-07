9dc295840ff1ed3bcad4d437669aba3c9d48087f
Ian Hundere <ianfundere@gmail.com>
2020-10-07 00:10:32 -0400

    Big.

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ const Header = (props) => (
     <header id="header" style={props.timeout ? { display: 'none' } : {}}>
         <div className="content">
             <div className="inner">
-                <h2>Ian Hundere</h2>
+                <h1>Ian Hundere</h1>
                 <sub>
                     DevOps
                     <sub>
